8515361cd42fc3e9c8c47855fa831f854e9494df
Shaikh-Danish <shaikhsaalim327@gmail.com>
2024-01-24 12:50:57 +0530

    fix: collection name from env instead of hard code

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/attendance_prod`, {})
+    const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${process.env.COLLECTION_NAME}`, {})
     console.log(`MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`);
   } catch(err) {
     console.error("Error connecting to MongoDB:", err);
